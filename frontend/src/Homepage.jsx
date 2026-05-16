d60567178fa56b72cad8fa4f9e58e05088ffa246
Arunshaw-git <arunshaw08@gmail.com>
2026-05-16 06:05:48 +0530

    new bg

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./stles/homepage.css";
 import SentimentCard from "./SentimentCard";
-import heroBg from "/herobg.jpeg";
+import heroBg from "/liquid-bg.png";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
